63ee5d31fe37d06fab99d0d685f86c5b6af48d1c
modo <vesingh@student.wethinkcode.co.za>
2019-07-15 06:55:36 +0200

    push_swap.h

---
+++ prac/push_swap.h
@@ -0,0 +1,17 @@
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+typedef struct	int_list
+{
+	int				a;
+	struct int_list	*next;
+}				a_list;
+
+
+
+#endif
